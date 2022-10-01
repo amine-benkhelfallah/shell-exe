cf65ed0f5087b74b9f28d1ba19e73b27d7429e3f
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-10-01 02:10:38 +0200

    modification compte rendu script

--- shell-exe/job8/get_logs.sh
+++ shell-exe/job8/get_logs.sh
@@ -5,9 +5,15 @@ nameoffile="number_connection-"$date
 echo $nameoffile
 
 last a | grep -c a > ~/shell-exe/job8/$nameoffile
+
 mkdir -p ~/shell-exe/job8/Backup
-tar -cvf "Backup/$nameoffile.tar" $nameoffile
+tar -cvf ~/shell-exe/job8/Backup/$nameoffile.tar $nameoffile
 rm ~/shell-exe/job8/$nameoffile
+
+
+
+#le reglage de cron se fait avec crontab -e en tapant la commande suivante 
+# 0 */1 * * * ~/shell-exe/job8/get_logs.sh
 
 
 
